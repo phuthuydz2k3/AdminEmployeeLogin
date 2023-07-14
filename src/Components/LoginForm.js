b4c0bbe8fbb5f17c639fb54f2894799a6bddb95d
phuthuydz2k3 <hnhtr1999@gmail.com>
2023-07-14 17:30:50 +0700

    new commit

--- src/Components/LoginForm.js
+++ src/Components/LoginForm.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useRef} from 'react';
 import { useNavigate } from 'react-router-dom';
 import Design from './img/download.svg';
 import Design2 from './img/download1.svg';
@@ -7,6 +7,10 @@ const LoginForm = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
+    let role = useRef('');
+    const alert = document.getElementById("alert");
+    const form = document.getElementById("form");
+
     useEffect(() => {
         const card1 = document.getElementById("card1");
         const card2 = document.getElementById("card2");
@@ -14,6 +18,8 @@ const LoginForm = () => {
         const switch2 = document.getElementById("switch2");
         const button1 = document.getElementById("button1");
         const button2 = document.getElementById("button2");
+        const text1 = document.getElementById("text1");
+        const text2 = document.getElementById("text2");
 
         const handleButton1Click = () => {
             card1.classList.add("open");
@@ -22,6 +28,9 @@ const LoginForm = () => {
             switch1.style.display = "block";
             button1.style.display = "none";
             button2.style.display = "none";
+            text1.style.display = "block";
+            text2.style.display = "block";
+            role.current.value = 'admin';
         };
 
         const handleButton2Click = () => {
@@ -32,12 +41,16 @@ const LoginForm = () => {
             switch1.style.display = "block";
             button1.style.display = "none";
             button2.style.display = "none";
+            text1.style.display = "block";
+            text2.style.display = "block";
+            role.current.value = "employee"
         };
 
         const handleSwitch1Click = () => {
             card1.style.display = "none";
             card2.style.display = "block";
             switch2.style.display = "block";
+            role.current.value = "employee";
         };
 
 
@@ -45,6 +58,7 @@ const LoginForm = () => {
             card2.style.display = "none";
             card1.style.display = "block";
             switch1.style.display = "block";
+            role.current.value = "admin"
         }
 
         button1.addEventListener("click", handleButton1Click);
@@ -70,6 +84,16 @@ const LoginForm = () => {
 
     const handleSubmit = (event) => {
         event.preventDefault();
+
+        if (role.current === "")
+        {
+            alert.style.display = "block";
+            form.classList.add("disabled-overlay");
+            document.body.addEventListener("click", () => {
+                form.classList.remove("disabled-overlay")
+                alert.style.display = "none";
+            })
+        }
 
         // Perform login logic (e.g., send a request to the server with username and password)
         // On success, set user session or redirect to a protected page
@@ -100,43 +124,81 @@ const LoginForm = () => {
 
     return (
     <div className="mx-auto max-w-screen-xl px-4 py-16 sm:px-6 lg:px-8">
-        <div className="form">
+        <div role="alert" id="alert" className="absolute rounded border-s-4 border-red-500 bg-red-50 p-4">
+            <div className="flex items-center gap-2 text-red-800">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    className="h-5 w-5"
+                >
+                    <path
+                        fillRule="evenodd"
+                        d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
+                        clipRule="evenodd"
+                    />
+                </svg>
+
+                <strong className="block font-medium"> You need to choose your role </strong>
+            </div>
+
+            <p className="mt-2 text-sm text-red-700">
+                Choose a role by click in the according button on the screen.
+            </p>
+        </div>
+        <div className="form" id="form">
             <div className="cards">
                 <div href="" className="card group relative block h-64 sm:h-80 lg:h-96" id="card1">
                     <div
                         id="dash1"
-                        className="relative flex h-full transform items-end border-2 border-black bg-white rounded-2xl"
+                        className="relative flex h-full transform items-end shadow-lg rounded-lg"
                     >
                         <div
                             id="info1"
                             className="absolute p-4 group-hover:relative sm:p-6 lg:p-8"
                         >
-                            <p className="relative" id="switch1">Switch to Employee</p>
+                            <p className="relative transition hover:scale-110 focus:outline-none focus:ring" id="switch1">Switch to Employee</p>
                             <img src={Design2} alt="SVG Image"/>
-                            <a id="button1"
-                                className="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+                            <div id="button1"
+                                 className="group relative inline-block text-sm font-medium text-white focus:outline-none focus:ring"
                             >
+                              <span
+                                  className="absolute inset-0 border border-blue-600 group-active:border-blue-500 rounded-lg"
+                              ></span>
+                                <span
+                                    className="block border border-blue-600 bg-blue-600 rounded-lg px-12 py-3 transition-transform active:border-blue-500 active:bg-blue-500 group-hover:-translate-x-1 group-hover:-translate-y-1"
+                                >
                                 Sign in as Admin
-                            </a>
+                              </span>
+                            </div>
+                            <p className="relative" id="text1">Admin</p>
                         </div>
                     </div>
                 </div>
                 <div href="" className="card group relative block h-64 sm:h-80 lg:h-96" id="card2">
                     <div
                         id="dash2"
-                        className="dash relative flex h-full transform items-end border-2 border-black bg-white rounded-2xl"
+                        className="dash relative flex h-full transform items-end shadow-lg rounded-lg"
                     >
                         <div
                             id="info2"
                             className="absolute p-4 group-hover:relative sm:p-6 lg:p-8"
                         >
-                            <p className="relative" id="switch2">Switch to Admin</p>
+                            <p className="relative transition hover:scale-110 focus:outline-none focus:ring" id="switch2">Switch to Admin</p>
                             <img src={Design} />
-                            <a id="button2"
-                                className="inline-block rounded border border-indigo-600 bg-indigo-600 px-12 py-3 text-sm font-medium text-white hover:bg-transparent hover:text-indigo-600 focus:outline-none focus:ring active:text-indigo-500"
+                            <div id="button2"
+                                className="group relative inline-block text-sm font-medium text-white focus:outline-none focus:ring"
                             >
+                              <span
+                                  className="absolute inset-0 border border-blue-600 group-active:border-blue-500 rounded-lg"
+                              ></span>
+                                <span
+                                    className="block border border-blue-600 bg-blue-600 rounded-lg px-12 py-3 transition-transform active:border-blue-500 active:bg-blue-500 group-hover:-translate-x-1 group-hover:-translate-y-1"
+                                >
                                 Sign in as Employee
-                            </a>
+                              </span>
+                            </div>
+                            <p className="relative" id="text2">Employee</p>
                         </div>
                     </div>
                 </div>
@@ -157,24 +219,25 @@ const LoginForm = () => {
                             type="name"
                             className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
                             value={username} onChange={handleUsernameChange} placeholder="Username"
+                            required
                         />
 
                         <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-4 w-4 text-gray-400"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-            >
-              <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
-              />
-            </svg>
-          </span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-4 w-4 text-gray-400"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                              <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+                              />
+                            </svg>
+                        </span>
                     </div>
                 </div>
 
@@ -186,6 +249,7 @@ const LoginForm = () => {
                             type="password"
                             className="w-full rounded-lg border-gray-200 p-4 pe-12 text-sm shadow-sm"
                             value={password} onChange={handlePasswordChange} placeholder="Password"
+                            required
                         />
 
                         <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
@@ -215,7 +279,7 @@ const LoginForm = () => {
 
                 <button
                     type="submit"
-                    className="block w-full rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white"
+                    className="block w-full rounded-lg bg-blue-600 px-5 py-3 text-sm font-medium text-white"
                 >
                     Sign in
                 </button>
